e61f47442ef1e31befd11c568a0f5d3e1b39a343
Pablo Rengifo <delasota00@gmail.com>
2024-12-20 13:23:51 +0000

    test 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (first < 0 || second < 0) {
+        return "ERROR";
+    }
+    if (first > second) {
+        let temp = first;
+        first = second;
+        second = temp;
+    }
+    let sum = 0;
+    for(let i = first; i <= second; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
