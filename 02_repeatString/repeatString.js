2b50e5f7cfa77662ea4436b5557cc749376d6458
Pablo Rengifo <delasota00@gmail.com>
2024-12-20 10:48:58 +0000

    test exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(name, number) {
+    let result = '';
+    for(let i = 0; i < number; i++) {
+        result += name;
+    }
+    return result;
 };
 
 // Do not edit below this line
