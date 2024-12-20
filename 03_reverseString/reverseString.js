23be83d51d7732d4fe6775596ceaf7d3539acec4
Pablo Rengifo <delasota00@gmail.com>
2024-12-20 11:46:43 +0000

    test 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(name) {
+    let result = "";
+    for(let i = (name.length - 1); i >= 0; i--) {
+        result += name.charAt(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
