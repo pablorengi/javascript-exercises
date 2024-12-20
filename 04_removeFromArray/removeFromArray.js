75191a568e51bb18f8a2681ba4a8d0f638c9aafc
Pablo Rengifo <delasota00@gmail.com>
2024-12-20 12:15:20 +0000

    test 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...values) {
+    for(let i = 0; i < myArray.length; i++) {
+        if(values.includes(myArray[i])) {
+            myArray.splice(i, 1);
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
